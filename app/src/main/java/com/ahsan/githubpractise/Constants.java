05e6a56fb7c67b74d59420c2a3645d4905f796b6
Ahsan <ahsan.mohammad@tplholdings.com>
2018-05-07 17:59:20 +0500

    Initial commit

---
+++ app/src/main/java/com/ahsan/githubpractise/Constants.java
@@ -0,0 +1,14 @@
+package com.ahsan.githubpractise;
+
+/**
+ * Created by ahsan.mohammad on 5/7/2018.
+ */
+
+public class Constants {
+
+    public static final String BROADCAST_DETECTED_ACTIVITY = "activity_intent";
+
+    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 30 * 1000;
+
+    public static final int CONFIDENCE = 70;
+}
